96242d7b1a7fe4bb928329a15d02110db16e442f
Jan Krišica <jan.krisica@citrix.com>
2021-10-25 13:51:31 +0200

    MAPI-116 Synchronize only active users

--- bundles/identity_provider/Citrix/e7971891-608e-46be-956e-a3319c587bad/c79dc643-3fa0-482e-bb12-8bb8441b2186.js
+++ bundles/identity_provider/Citrix/e7971891-608e-46be-956e-a3319c587bad/c79dc643-3fa0-482e-bb12-8bb8441b2186.js
@@ -1,5 +1,5 @@
 const {User, Group, UserGroupMapping} = library.load("microapp-user-groups")
-const uuid = library.load("uuid")
+const {pick} = library.load("lodash");
 
 function fullSync(params) {
     fullSyncUsers(params)
@@ -7,123 +7,140 @@ function fullSync(params) {
 }
 
 function fullSyncUsers({client, dataStore}) {
-    var searchParameters = {
+    const searchParameters = {
         "startAt": 0,
         "maxResults": 50
-    }
-    let pageUserCount = 0
+    };
 
+    let pageUserCount = 0;
+    let currentResult;
     do {
-        console.log(`fullSyncUsers(startAt=${searchParameters.startAt})`)
-        const response = client.fetchSync(`/rest/api/2/users/search?username=.&startAt=${searchParameters.startAt}&maxResults=${searchParameters.maxResults}`)
-        if (!response.ok) {
-            const errorMessage = `Users sync failed ${response.status}:${response.statusText}.`
-            console.log(errorMessage)
-            console.log(`Error body: ${response.textSync()}`)
-            throw new Error(errorMessage)
-        } else {
-            let responseUsers = response.jsonSync()
-            pageUserCount = responseUsers.length
-            console.log(`Users on page=${pageUserCount} offset=${searchParameters.startAt}`)
-            console.log(`Users received data: (${JSON.stringify(responseUsers)})`)
+        console.log(`fullSyncUsers(startAt=${searchParameters.startAt})`);
+        const response = client.fetchSync(`/rest/api/2/users/search?username=.&startAt=${searchParameters.startAt}&maxResults=${searchParameters.maxResults}`);
 
-            // TODO missing mandatory email?
-            let users = responseUsers.map(user => {
-                const userId = trimIdPrefix(user.accountId)
-                const email = user.emailAddress ?? userId
-                return new User(user.displayName, user.displayName, email, "domain", user.name, userId)
+        if (!response.ok) {
+            const errorMessage = `Users sync failed ${response.status}:${response.statusText}.`;
+            console.log(errorMessage);
+            console.log(`Error body: ${response.textSync()}`);
+            throw new Error(errorMessage);
+        }
+
+        currentResult = response.jsonSync();
+
+        pageUserCount = currentResult.length;
+        console.log(`Users on page=${pageUserCount}`);
+        console.log(`Users received data: (${JSON.stringify(currentResult)})`);
+
+        const users = currentResult
+            .filter(user => user.active)
+            .map(user => {
+                // Account id contains ID that is longer than destination table user with max 36 chars for userId
+                // "accountId": "557058:73ba846a-2e13-4020-9898-57cfab0e00b9",
+                const userId = trimIdPrefix(user.accountId);
+                // Some users has missing mandatory email
+                const email = user.emailAddress ?? userId;
+                // Domain value not set for user
+                return new User(user.displayName, user.displayName, email, null, user.name, userId);
             })
 
-            console.log(`Users data: (${JSON.stringify(users)})`)
-            dataStore.save(User.tableModel, users)
-        }
+        console.log(
+            `Saving ${users.length} users to data store, ${JSON.stringify(
+                users.map(user => pick(user, ["user_id", "account_name", "email"]))
+            )}`
+        );
+        dataStore.save(User.tableModel, users);
+
         searchParameters.startAt = searchParameters.startAt + searchParameters.maxResults
 
-    } while (pageUserCount == searchParameters.maxResults) //TODO
-
-    console.log(`Total users=${searchParameters.startAt}`)
-}
-
-function trimIdPrefix(id) {
-    const elementPosition = id.indexOf(":")
-    return elementPosition != -1 ? id.slice(elementPosition + 1) : id
+    } while (pageUserCount == searchParameters.maxResults); //TODO
 }
 
 function fullSyncGroups({client, dataStore}) {
-    var searchParameters = {
+    const searchParameters = {
         "startAt": 0,
         "maxResults": 50
-    }
+    };
 
+    let currentResult;
     do {
-        console.log(`fullSyncGroups(startAt=${searchParameters.startAt})`)
-        var response = client.fetchSync("/rest/api/2/group/bulk")
+        console.log(`fullSyncGroups(startAt=${searchParameters.startAt})`);
+        const response = client.fetchSync("/rest/api/2/group/bulk");
 
         if (!response.ok) {
-            const errorMessage = `Groups sync failed ${response.status}:${response.statusText}.`
-            console.log(errorMessage)
-            console.log(`Error body: ${response.textSync()}`)
-            throw new Error(errorMessage)
-        } else {
-            responseGroups = response.jsonSync()
-            console.log(`Group response received, total: ${responseGroups.total}`)
-            console.log(`Group received data: (${JSON.stringify(responseGroups)})`)
-
-            let groups = responseGroups.values.map(group => {
-                return new Group(group.name, group.name, "domain", group.name, group.groupId, null)
-            })
-
-            console.log(`Saving ${groups.length} to data store`)
-
-            dataStore.save(Group.tableModel, groups)
-            console.log(`Groups data: (${JSON.stringify(groups)})`)
-            groups.map(group => {
-                console.log(`Group account_name=${group.account_name} group_id=${group.group_id}`)
-                userGroupMapping(client, dataStore, group)
-            })
-            searchParameters.startAt += searchParameters.maxResults
+            const errorMessage = `Groups sync failed ${response.status}:${response.statusText}.`;
+            console.log(errorMessage);
+            console.log(`Error body: ${response.textSync()}`);
+            throw new Error(errorMessage);
         }
-    } while (searchParameters.startAt < responseGroups.total)
+
+        currentResult = response.jsonSync();
+        console.log(`Group response received, total: ${currentResult.total}`);
+        console.log(`Group received data: (${JSON.stringify(currentResult)})`);
+
+        const groups = currentResult.values.map(group =>
+            new Group(group.name, group.name, null, group.name, group.groupId, null));
+
+        console.log(`Saving ${groups.length} groups to data store, ${JSON.stringify(groups)}`);
+        dataStore.save(Group.tableModel, groups);
+
+        groups.map(group => {
+            console.log(`Group ${JSON.stringify(pick(group, ["account_name", "group_id"]))}`);
+            mapUserGroup(client, dataStore, group)
+        });
+
+        searchParameters.startAt += searchParameters.maxResults;
+
+    } while (searchParameters.startAt < currentResult.total);
 }
 
-function userGroupMapping(client, dataStore, group) {
-    var searchParameters = {
+function mapUserGroup(client, dataStore, group) {
+    const searchParameters = {
         "startAt": 0,
-        "maxResults": 5
-    }
+        "maxResults": 50
+    };
 
+    let currentResult;
     do {
-        console.log(`userGroupMapping(startAt=${searchParameters.startAt})`)
-        var response = client.fetchSync(`/rest/api/2/group/member?groupname=${group.account_name}&startAt=${searchParameters.startAt}&maxResults=${searchParameters.maxResults}`)
-        //TODO ?account_name
+        console.log(`userGroupMapping(startAt=${searchParameters.startAt})`);
+        const response = client.fetchSync(`/rest/api/2/group/member?groupname=${group.account_name}
+        &startAt=${searchParameters.startAt}
+        &maxResults=${searchParameters.maxResults}`);
 
         if (!response.ok) {
-            const message = `Groups sync failed ${response.status}:${response.statusText}.`
-            console.log(message)
-            console.log(`Error body: ${response.textSync()}`)
-            return
+            const message = `User group mapping sync failed ${response.status}:${response.statusText}.`;
+            console.log(message);
+            console.log(`Error body: ${response.textSync()}`);
+            // Some of the queries for group return HTTP 404
+            return;
         } else {
-            responseMappings = response.jsonSync()
-            console.log(`Group mappings response received, total: ${responseMappings.total}`)
-            console.log(`Group received data: (${JSON.stringify(responseMappings)})`)
+            currentResult = response.jsonSync();
+            console.log(`User group mappings response received, total: ${currentResult.total}`);
+            console.log(`User group mappings data: (${JSON.stringify(currentResult)})`);
 
-            let userGroupMappings = responseMappings.values.map(user => {
-                return new UserGroupMapping(group.group_id, trimIdPrefix(user.accountId))
-            })
+            const userGroupMappings = currentResult.values
+                .filter(user => user.active)
+                .map(user => {
+                    return new UserGroupMapping(group.group_id, trimIdPrefix(user.accountId))
+                });
 
-            console.log(`Group mappings data: (${JSON.stringify(userGroupMappings)})`)
-            console.log(`Saving ${userGroupMappings.length} to data store`)
-            dataStore.save(UserGroupMapping.tableModel, userGroupMappings)
-
+            console.log(
+                `Saving ${userGroupMappings.length} userGroupMapping to data store, ${JSON.stringify(userGroupMappings)}`
+            );
+            dataStore.save(UserGroupMapping.tableModel, userGroupMappings);
         }
-        searchParameters.startAt += searchParameters.maxResults
-    } while (searchParameters.startAt < responseMappings.total)
+        searchParameters.startAt += searchParameters.maxResults;
+    } while (searchParameters.startAt < currentResult.total);
+}
+
+const trimIdPrefix = (id) => {
+    const elementPosition = id.indexOf(":");
+    return elementPosition !== -1 ? id.slice(elementPosition + 1) : id;
 }
 
 integration.define({
     synchronizations: [
         {
-            name: "jiraUserGroups", // Logical name
+            name: "jiraUserGroups",
             fullSyncFunction: fullSync,
         }
     ],
